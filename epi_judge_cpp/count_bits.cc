9e30ad62c3469dfef8eae496d47a98c53c5a54bd
code4n <suman.sunkara@gmail.com>
2018-02-03 10:42:21 -0800

    Count Bits

--- epi_judge_cpp/count_bits.cc
+++ epi_judge_cpp/count_bits.cc
@@ -1,7 +1,12 @@
 
 short CountBits(unsigned int x) {
-  // Implement this placeholder.
-  return 0;
+  // Iterate over each bit
+	short count = 0;
+	while (x) {
+		count += x & 1;
+		x = x >> 1;
+	}
+	return count;
 }
 
 #include "test_framework/test_utils_generic_main.h"
